d2e613ef6dc82738790713bbf9c3b79906644128
ozzlemdemir <146757725+ozzlemdemir@users.noreply.github.com>
2025-09-24 14:14:15 +0300

    Pyhton-Deişkenler

---
+++ Desktop/1saattePyhton/HelloWorld/Degiskenler.py
@@ -0,0 +1,15 @@
+x=3 #int
+isim="Özlem" #string
+fiyat=13.95 #float
+Aldimi=True #boolean
+print(isim,fiyat,"üründen",x,"adet aldı")
+#alıştırma
+name= "Kütüphane"
+book="Moby Dick"
+sayfa=195
+agirlik= 13.45
+YeniMi=True
+if YeniMi:
+    YeniMi="Yeni"
+
+print(name,"'e",book,"adında",sayfa,"sayfalık ve",agirlik,"ağırlığında",YeniMi,"kitap eklendi")
